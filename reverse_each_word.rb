6d2eddb4de7a9f0f79d0a2eafbd05dac08ba39ed
ILJOO CHAE <9662964@gmail.com>
2020-04-17 14:53:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string)
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
